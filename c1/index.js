22565cd9221aedbf572014e38b69370191a2a61a
KristijanGjorevski <kikogjorevski@gmail.com>
2017-11-16 13:29:36 +0100

    Java cas 1

---
+++ c1/index.js
@@ -0,0 +1,25 @@
+console.log("brrrrra");
+
+var ime = "Kiko";  // string ( niza od karakteri  { alfa numericki karakteri i znaci } )
+var godini = 35;   // number
+var visina = 173.3; // number
+var temperatura = -22.5; //number 
+var classEnded = true; // boolean
+
+console.log(ime);
+console.log(classEnded);
+
+godinaNaRaganje = 1993; // camel case style
+godina_na_ragjanje = 1993 // snake case style
+godinanaragjanje = 1993 // bad case style! BAD CASE !
+
+
+//	"..."
+//	'...'
+//	`...`
+
+console.log(x + ' ' + y);
+console.log(ime + '' + prezime);
+
+variablite ne smeat da pocnat so brojka
+variablite mozat da se pocnat so : a-z , A-Z , _ $;
