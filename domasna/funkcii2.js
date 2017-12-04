ff03e100cdb4cb2a39fbf5caad318e210df867ee
KristijanGjorevski <kikogjorevski@gmail.com>
2017-12-04 16:13:47 +0100

    WebStorm

--- domasna/funkcii2.js
+++ domasna/funkcii2.js
@@ -1,14 +1,33 @@
-var niza5 = [2, 5, 3, 2, 3, 3, 5, 1, 1, 4, 4, 2, 4, 2, 2, 4];
+var niza5 = [2, 5, 3, 2, 3, 3, 5, 1, 1, 4, 4, 2, 4, 2, 2, 4, 2];
 var x = 0;
 var brojac = 0;
 
 for (i = 0;  i<niza5.length; i++) {
-	if ( niza5[i] % 2 == 0) {
+	if ( niza5[i] % 2 === 0) {
 		brojac++;
-		if(brojac == 3){
+		if(brojac === 3){
 			console.log(niza5[i]);
 			brojac = 0;
 		}
 	}
 }
+
+//nacin 2
+brojac = 0;
+niza5.forEach(function (value) {
+	//console.log("index : " +index + " value : " +value);
+    //
+    if ( value % 2 === 0) {
+        brojac++;
+        if(brojac === 3){
+            console.log(value);
+            brojac = 0;
+        }
+    }
+
+});
+
+niza5.forEach(function(value,index){
+
+});
 
