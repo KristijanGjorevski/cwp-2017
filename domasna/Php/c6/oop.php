7a0dfcf8c0bd792c2885e5cf73e04e7acdfed35c
KristijanGjorevski <kikogjorevski@gmail.com>
2018-03-15 21:25:10 +0100

    upd

--- domasna/Php/c6/oop.php
+++ domasna/Php/c6/oop.php
@@ -2,12 +2,41 @@
 
 class Person{
 
-    private
+    private $name;
+    private $lastName
+    private $email;
+    private$password;
 
-    public function __construct()
+    public function __construct($name,$email,$password)
     {
-        
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
     }
+
+    public function setMail($email){
+        $this->email = $email;
+        echo "mail has been set up";
+    }
+    public function getMail(){
+        return $this->email;
+    }
+    public function setName($name){
+        $this->name = $name;
+        echo "Name has been set up";
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function setPassword($password){
+        $this->password = $password;
+        echo "Password has been set up";
+    }
+    public function getPassword(){
+        return $this->password;
+
+    }
+
 
 
 }
